3517254ebb25a20e61b24d53b03036ab41ae90ba
angusj <angusj@758e7f8d-410f-429b-b4f6-457a56196ea1>
2021-01-27 12:39:18 +0000

    Updated offsetting in Clipper2 (sandbox)

--- Clipper2/cpp/clipper_offset.h
+++ Clipper2/cpp/clipper_offset.h
@@ -14,8 +14,8 @@
 #define CLIPPER_OFFSET_H_
 
 #include "clipper.h"
-#include "clipper_core.h"
-#include <list>
+#include "clipper_core.hpp"
+//#include <list>
 
 namespace clipperlib {
 
@@ -30,70 +30,70 @@ enum class EndType {Polygon, Joined, Butt, Square, Round};
 
 class PathGroup {
 public:
-    PathsD paths;
-    JoinType join_type;
-    EndType end_type;
-    PathGroup(const PathsD &paths, JoinType join_type, EndType end_type):
-        paths(paths), join_type(join_type), end_type(end_type) {}
+	PathsD paths;
+	JoinType join_type;
+	EndType end_type;
+	PathGroup(const PathsD &paths, JoinType join_type, EndType end_type):
+		paths(paths), join_type(join_type), end_type(end_type) {}
 };
 
 class ClipperOffset {
 private:
-    double delta = 0.0;
-    double sin_val = 0.0;
-    double cos_val = 0.0;
-    double miter_lim = 0.0; 
-    double miter_limit = 0.0;
-    double steps_per_rad = 0.0;
-    PathD norms;
-    std::vector<PathGroup*> group_in;
-    PathD path_in;
-    PathD path_out;
-    PathsD paths_out;
-    PathsD solution;
-    JoinType join_type = JoinType::Square;
-    double arc_tolerance = 0.0;
-    double min_edge_len_sqrd = 0.0;
-    double min_edge_len = 0.25;
-    double scale_ = 1.0;
-    bool merge_groups = false;
-    inline void AddPoint(double x, double y) { path_out.push_back(PointD(x, y)); };
-    inline void AddPoint(PointD pt) { path_out.push_back(pt); };
-    void DoSquare(size_t j, size_t k);
-    void DoMiter(size_t j, size_t k, double cosAplus1);
-    void DoRound(size_t j, size_t k, double angle);
-    void BuildNormals();
-    void OffsetPolygon();
-    void OffsetOutline();
-    void OffsetOpenLine(EndType et);
-    void OffsetPoint(size_t j, size_t &k);
-    void DoOffset(PathGroup &pathGroup, double delta_);
+	double delta = 0.0;
+	double sin_val = 0.0;
+	double cos_val = 0.0;
+	double miter_lim = 0.0; 
+	double miter_limit = 0.0;
+	double steps_per_rad = 0.0;
+	PathD norms;
+	std::vector<PathGroup*> group_in;
+	PathD path_in;
+	PathD path_out;
+	PathsD paths_out;
+	PathsD solution;
+	JoinType join_type = JoinType::Square;
+	double arc_tolerance = 0.0;
+	double min_edge_len_sqrd = 0.0;
+	double min_edge_len = 0.25;
+	double scale_ = 1.0;
+	bool merge_groups = false;
+	inline void AddPoint(double x, double y) { path_out.push_back(PointD(x, y)); };
+	inline void AddPoint(PointD pt) { path_out.push_back(pt); };
+	void DoSquare(size_t j, size_t k);
+	void DoMiter(size_t j, size_t k, double cosAplus1);
+	void DoRound(size_t j, size_t k, double angle);
+	void BuildNormals();
+	void OffsetPolygon();
+	void OffsetOutline();
+	void OffsetOpenLine(EndType et);
+	void OffsetPoint(size_t j, size_t &k);
+	void DoOffset(PathGroup &pathGroup, double delta_);
 public:
-    ClipperOffset(double miter_limit_ = 2.0, double arc_tolerance_ = 0.0) :
-        miter_limit(miter_limit_), arc_tolerance(arc_tolerance_) {};
-    ~ClipperOffset() { Clear(); };
+	ClipperOffset(double miter_limit_ = 2.0, double arc_tolerance_ = 0.0) :
+		miter_limit(miter_limit_), arc_tolerance(arc_tolerance_) {};
+	~ClipperOffset() { Clear(); };
 
-    void AddPath(const PathD &p, JoinType jt_, EndType et_);
-    void AddPaths(const PathsD &p, JoinType jt_, EndType et_);
-    void Clear();
-    PathsD Execute(double delta_);
+	void AddPath(const PathD &p, JoinType jt_, EndType et_);
+	void AddPaths(const PathsD &p, JoinType jt_, EndType et_);
+	void Clear();
+	PathsD Execute(double delta_);
 
-    //ArcTolerance: needed for rounded offsets (See offset_triginometry2.svg)
-    void ArcTolerance(double arc_tolerance_) { arc_tolerance = arc_tolerance_; }
-    //MergeGroups: A path group is one or more paths added via the AddPath or
-    //AddPaths methods. By default these path groups will be offset
-    //independently of other groups and this may cause overlaps (intersections).
-    //However, when MergeGroups is enabled, any overlapping offsets will be
-    //merged (via a clipping union operation) to remove overlaps.
-    void MergeGroups(bool merge_groups_) { merge_groups = merge_groups_; }
-    //MinEdgeLength: Very small edges are often at unexpected angles and these
-    //can become visible artefacts with offsetting. 
-    //Consequently it's best to remove these tiny edges before processing.
-    void MinEdgeLength(double min_edge_len_) { min_edge_len = min_edge_len_; }
+	//ArcTolerance: needed for rounded offsets (See offset_triginometry2.svg)
+	void ArcTolerance(double arc_tolerance_) { arc_tolerance = arc_tolerance_; }
+	//MergeGroups: A path group is one or more paths added via the AddPath or
+	//AddPaths methods. By default these path groups will be offset
+	//independently of other groups and this may cause overlaps (intersections).
+	//However, when MergeGroups is enabled, any overlapping offsets will be
+	//merged (via a clipping union operation) to remove overlaps.
+	void MergeGroups(bool merge_groups_) { merge_groups = merge_groups_; }
+	//MinEdgeLength: Very small edges are often at unexpected angles and these
+	//can become visible artefacts with offsetting. 
+	//Consequently it's best to remove these tiny edges before processing.
+	void MinEdgeLength(double min_edge_len_) { min_edge_len = min_edge_len_; }
 };
 
-PathsI InflatePaths(const PathsI paths, double delta, JoinType jt, EndType et);
-PathsD InflatePaths(const PathsD paths, double delta, JoinType jt, EndType et);
+PathsI InflatePaths(const PathsI &paths, double delta, JoinType jt, EndType et);
+PathsD InflatePaths(const PathsD &paths, double delta, JoinType jt, EndType et);
 
 const double default_arc_frac = 0.025;
 const double two_pi = 2.0 * PI;
